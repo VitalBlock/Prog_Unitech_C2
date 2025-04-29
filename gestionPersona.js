1d3f522d273f191bded08afdf81f9453c881cf90
VitalBlock <olaxxf@gmail.com>
2025-04-29 04:46:06 -0500

    Update

--- gestionPersona.js
+++ gestionPersona.js
@@ -8,17 +8,19 @@ function guardarDatos(){
     objPersona.ape = document.getElementById('ape').value;
     objPersona.dir = document.getElementById('dir').value;
     objPersona.tel = document.getElementById('tel').value;
-    objPersona.email = document.getElementById('email').value;
+    objPersona.email = document.getElementById('cor').value;
     myArray.push(objPersona);
+    console.log(myArray);
     limpiarCajas();
     mostrarDatos();
 }
 function limpiarCajas(){
+    alert('Los datos se guardaron correctamente');
     document.getElementById('ced').value = '';
     document.getElementById('nom').value = '';
     document.getElementById('ape').value = '';
     document.getElementById('dir').value = '';
     document.getElementById('tel').value = '';
-    document.getElementById('email').value = '';
+    document.getElementById('cor').value = '';
     document.getElementById('ced').focus();
 }   
